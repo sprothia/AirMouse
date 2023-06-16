698e1906f9ebbb08012c60288db8260628cea22d
sprothia <79033000+sprothia@users.noreply.github.com>
2023-06-16 13:08:35 -0700

    Updated website.py

--- website.py
+++ website.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template
 import subprocess
 import sys
 
@@ -7,11 +7,6 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
-
-@app.route('/', methods=['POST'])
-def run_script():
-    subprocess.run([f"{sys.executable}", 'MacControl.py'])
-    return redirect(url_for('home'))
 
 if __name__ == "__main__":
     app.run(debug=False, port=5000)
